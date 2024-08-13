f50371242237e8b1b1007dc7fe6cbb2d8125446f
UriMakarov <umakarov@colvir.com>
2024-08-13 14:27:36 +0300

    getUsersByTaskId

--- src/main/java/com/umakarov/JavaCourseWork/repository/UserRepository.java
+++ src/main/java/com/umakarov/JavaCourseWork/repository/UserRepository.java
@@ -1,7 +1,15 @@
 package com.umakarov.JavaCourseWork.repository;
 
+import com.umakarov.JavaCourseWork.entity.Task;
 import com.umakarov.JavaCourseWork.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository  extends JpaRepository <User, Long> {
+    @Query(nativeQuery = true
+            , value = "SELECT u.id, u.first_name, u.last_name, u.department " +
+            "FROM users u, user_tasks ut WHERE u.id = ut.user_id AND ut.task_id = :id")
+    List<Task> getUsersByTaskId(Long id);
 }
